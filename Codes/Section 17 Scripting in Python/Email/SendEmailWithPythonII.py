d6bc3c4caf34fe7f6b55dd36cefc6475bd72be83
Gursewak Singh Sidhu <54534337+Sidhus234@users.noreply.github.com>
2021-04-15 07:34:33 +0800

    Add files via upload

---
+++ Codes/Section 17 Scripting in Python/Email/SendEmailWithPythonII.py
@@ -0,0 +1,30 @@
+# https://treyhunner.com/2018/12/why-you-should-be-using-pathlib/
+
+# EMAIL Module: https://docs.python.org/3/library/email.examples.html
+# EMAIL Module legacy: https://docs.python.org/3/library/email.html#module-email
+# SMTP: https://www.geeksforgeeks.org/simple-mail-transfer-protocol-smtp/
+import smtplib
+from email.message import EmailMessage
+from string import Template
+from pathlib import Path # very similar to os.path 
+
+# To send email, we need to set up a server. smtplib is used to set up a server
+# for communication
+
+# SMTP: Simple Mail Transfer Protocol
+
+html = Template(Path('index.html').read_text())
+
+email = EmailMessage()
+email['from'] = 'Gursewak Sidhu'
+email['to'] = 'sidhus234@gmail.com'
+email['subject'] = 'You won 1,000,000 dollars'
+email.set_content(html.substitute({'name':'TinTin'}),'html')
+
+with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
+    smtp.ehlo() #hello message, telling server is awake
+    smtp.starttls() # encryption method
+    smtp.login('gs3529688@gmail.com', 'Apple123#@!')
+    smtp.send_message(email)
+    print('All good boss')
+    
