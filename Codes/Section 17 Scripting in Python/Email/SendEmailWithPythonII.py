38cfac04b5b8e4d45b761a2ba632ac8f0ccc1308
Gursewak Singh Sidhu <54534337+Sidhus234@users.noreply.github.com>
2021-04-18 10:26:11 +0800

    Removed personal information

--- Codes/Section 17 Scripting in Python/Email/SendEmailWithPythonII.py
+++ Codes/Section 17 Scripting in Python/Email/SendEmailWithPythonII.py
@@ -17,14 +17,14 @@ html = Template(Path('index.html').read_text())
 
 email = EmailMessage()
 email['from'] = 'Gursewak Sidhu'
-email['to'] = 'sidhus234@gmail.com'
+email['to'] = 'xxxxxxxx@gmail.com'
 email['subject'] = 'You won 1,000,000 dollars'
 email.set_content(html.substitute({'name':'TinTin'}),'html')
 
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo() #hello message, telling server is awake
     smtp.starttls() # encryption method
-    smtp.login('gs3529688@gmail.com', 'Apple123#@!')
+    smtp.login('xxxxxxxxx@gmail.com', 'xxxxxx!')
     smtp.send_message(email)
     print('All good boss')
     
